0164b65a9178e8afb5f63cde4fa0d24119c6c1f9
aayushadeshara333 <adeshara.aayush@gmail.com>
2022-03-04 15:28:40 +0530

    Modified String Practical exercise1

--- string - practicals/exercise1.rb
+++ string - practicals/exercise1.rb
@@ -1,9 +1,9 @@
-arr = ['ab', 'a']
+arr = ["abab","aba",""]
 def longestCommonPrefix(arr)
     stack = arr[0].split("")
     p stack
     (1...arr.length).each do |x|
-        if arr[x].length <= stack.length
+        if arr[x].length > 0
             arr[x].split("").each_with_index do |each, index|
                 # p each
                 if each != stack[index]
@@ -14,9 +14,11 @@ def longestCommonPrefix(arr)
                     stack = arr[x].split("")
                 end
             end
+        else
+            return ""
         end
     end
-    return stack.empty? ? "Not Found" : stack.join("")
+    return stack.empty? ? "" : stack.join("")
 end
 answer = longestCommonPrefix(arr)
 p answer
